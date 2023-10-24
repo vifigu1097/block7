48605d81c5bf5da0ae7684fc98bf35057bd3db17
Linaa Finklea <sky@linaas-mbp.mynetworksettings.com>
2023-10-24 09:06:47 -0400

    good

---
+++ index.js
@@ -0,0 +1,11 @@
+// Create a string 
+// make sure to add  secret combination
+// assign my 3 variables 
+// use addition (+) to calculate 10
+// use multiplication (*) to calculate 40 
+// use subtraction (-) to calculate 39 
+// create dialog box with vault codes (10-40-39)
+
+
+const message= "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+ 
